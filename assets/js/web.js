70352bc1e6305bf3463952bbf45cf9c80346c026
Thannia Hernandez <thaherna1@gmail.com>
2024-04-04 18:49:59 -0500

    added to html

--- assets/js/web.js
+++ assets/js/web.js
@@ -13,7 +13,7 @@ fetch('http://www.omdbapi.com/?i=tt3896198&apikey=a8e2098d')
   .catch(error => {
     console.error('There was a problem with the fetch operation:', error);
   });
-//IMDB
+//IMDB-OT
   fetch('https://search.imdbot.workers.dev/?tt=tt13667402')
   .then(response => {
     if (!response.ok) {
